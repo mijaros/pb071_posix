90a70b1a35372d1028d6a1a7497163829b615c11
M Jaros <xjaros1@fi.muni.cz>
2015-04-21 15:10:47 +0200

    Implementation files added

---
+++ ukol1/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2) {
+    fprintf(stderr, "Invalid amount of arguments\n");
+    return -1;
+  }
+  int fd = open(argv[1], O_RDONLY);
+  if (fd < 0) {
+    perror(argv[1]);
+    return -1;
+  }
+
+  char buffer[1024] = {'\0'};
+  ssize_t size = 0;
+  while ( (size = read(fd, )) > 0) {
+    write(fd, buffer, 1024);
+  }
+
+  close(fd);
+  return 0;
+}
